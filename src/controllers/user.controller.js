b34cba7ed1d0b9c2e0cd2a688e0f368d5619d01f
devansh190804 <devanshpandit2004@gmail.com>
2025-07-13 13:18:23 +0530

    Add models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -176,8 +176,8 @@ const logoutUser = asyncHandler( async (req, res) =>{
    User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        undefined
+      $unset: {
+        refreshToken:1
             }
     },
     {
